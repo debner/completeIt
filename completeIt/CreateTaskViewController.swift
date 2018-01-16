5b2bacab3aa3a01a30b7cf8e4df4a3ea1656ea95
Daniel Debner <danny@debner.net>
2018-01-15 21:53:17 -0500

    Added UI Elements

---
+++ completeIt/CreateTaskViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateTaskViewController.swift
+//  completeIt
+//
+//  Created by Daniel Debner on 1/15/18.
+//  Copyright © 2018 Daniel Debner. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+	@IBOutlet weak var taskNameTextField: UITextField!
+	@IBOutlet weak var importantSwitch: UISwitch!
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	@IBAction func addTapped(_ sender: Any) {
+	}
+	
+}
---
+++ completeIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  completeIt
+//
+//  Created by Daniel Debner on 1/15/18.
+//  Copyright © 2018 Daniel Debner. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+	var name = ""
+	var important = false
+}
